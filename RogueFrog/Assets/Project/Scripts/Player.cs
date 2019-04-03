8104761cfc563c340163477a05ec93b6cc71041e
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-04-02 21:47:03 -0500

    Implement player movement using arrow keys/asdw

--- RogueFrog/Assets/Project/Scripts/Player.cs
+++ RogueFrog/Assets/Project/Scripts/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	// The size of a tile
+	public float JumpDistance = 0.32f;
+
+	private bool hasJumped;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,21 @@ public class Player : MonoBehaviour
     {
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
-        Debug.Log(horizontalMovement + " " + verticalMovement);
-    }
+
+		if (!hasJumped) {
+			// The frog didn't move
+			if (horizontalMovement != 0) {
+				transform.position = new Vector2(transform.position.x + (horizontalMovement > 0 ? JumpDistance : -JumpDistance), transform.position.y);
+				hasJumped = true;
+			}
+			if (verticalMovement != 0) {
+				transform.position = new Vector2(transform.position.x, transform.position.y + (verticalMovement > 0 ? JumpDistance : -JumpDistance));
+				hasJumped = true;
+			}
+		} else {
+			if (horizontalMovement == 0f && verticalMovement == 0f) {
+				hasJumped = false;
+			}
+		}
+	}
 }
