7f7d1cc136f78f411530f4d87ede6787184ee14c
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-04-04 05:36:54 -0500

    Add logic to keep player within the bounds of the map

--- RogueFrog/Assets/Project/Scripts/Player.cs
+++ RogueFrog/Assets/Project/Scripts/Player.cs
@@ -8,10 +8,11 @@ public class Player : MonoBehaviour
 	public float JumpDistance = 0.32f;
 
 	private bool hasJumped;
+	private Vector3 startingPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+		startingPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -34,6 +35,26 @@ public class Player : MonoBehaviour
 			if (horizontalMovement == 0f && verticalMovement == 0f) {
 				hasJumped = false;
 			}
+		}
+		// The logic below keeps the frog within the bounds of the map
+		// We divide by 100 to get the pixel value into Unity values
+		// REmember to make the 100 and 2 divisors into a float because Screen.height and width are ints
+		// and dividing an int by a float will ensure a float is returned
+		// Bottom bound
+		if (transform.position.y < -(Screen.height / 100f) / 2f) {
+			transform.position = new Vector2(transform.position.x, transform.position.y + JumpDistance);
+		}
+		// Top bound - Notice that we are moving the frog back to it's starting position
+		if (transform.position.y > (Screen.height / 100f) / 2f) {
+			transform.position = startingPosition;
+		}
+		// Left Bound
+		if (transform.position.x < -(Screen.width / 100f) / 2f) {
+			transform.position = new Vector2(transform.position.x + JumpDistance, transform.position.y);
+		}
+		// Right bound
+		if (transform.position.x > (Screen.width / 100f) / 2f) {
+			transform.position = new Vector2(transform.position.x - JumpDistance, transform.position.y);
 		}
 	}
 }
